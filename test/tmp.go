19f55f916b69d555d6e2f51f5f1974e7e79258b5
Ilya Suprun <pytok@yandex.ru>
2021-12-29 09:35:26 +0300

    modified:   cmd/server/main_test.go deleted:    storage/inmemory/memstorage_test.go modified:   test/tmp.go

--- test/tmp.go
+++ test/tmp.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"context"
 	"fmt"
 	"github.com/IlyaYP/devops/storage/inmemory"
 )
@@ -9,27 +8,27 @@ import (
 func testStore(st *inmemory.Storage) {
 	println(st)
 
-	if err := st.PutMetric(context.Background(), "gauge", "aaa", "111.111"); err != nil {
+	if err := st.PutMetric("gauge", "aaa", "111.111"); err != nil {
 		println(err.Error())
 		return
 	}
 
-	if err := st.PutMetric(context.Background(), "counter", "bbb", "222"); err != nil {
+	if err := st.PutMetric("counter", "bbb", "222"); err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	if err := st.PutMetric(context.Background(), "gauge", "ccc", "333.333"); err != nil {
+	if err := st.PutMetric("gauge", "ccc", "333.333"); err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	if v, err := st.GetMetric(context.Background(), "gauge", "ccc"); err != nil {
+	if v, err := st.GetMetric("gauge", "ccc"); err != nil {
 		fmt.Println(err.Error())
 		return
 	} else {
 		fmt.Println(v)
 	}
-	ret := st.ReadMetrics(context.Background())
+	ret := st.ReadMetrics()
 	fmt.Println(ret)
 
 }
