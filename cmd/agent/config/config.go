d7bad5177a01e342ae9ebb522ad03fc40fccb2f9
Ilya Suprun <pytok@yandex.ru>
2022-01-16 16:58:55 +0300

    improving agent config

---
+++ cmd/agent/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
+	"time"
+)
+
+//type config struct {
+//	Address        string `env:"ADDRESS" envDefault:"localhost:8080"`
+//	ReportInterval int    `env:"REPORT_INTERVAL" envDefault:"10"`
+//	PoolInterval   int    `env:"POLL_INTERVAL" envDefault:"2"`
+//}
+
+//type config struct {
+//	Address        string `env:"ADDRESS"`
+//	ReportInterval int    `env:"REPORT_INTERVAL"`
+//	PoolInterval   int    `env:"POLL_INTERVAL"`
+//}
+
+type Config struct {
+	Address        string        `env:"ADDRESS"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
+	PoolInterval   time.Duration `env:"POLL_INTERVAL"`
+	EndPoint       string
+}
+
+func LoadConfig() (*Config, error) {
+	var cfg Config
+	flag.StringVar(&cfg.Address, "a", "localhost:8080", "Server address")
+	flag.DurationVar(&cfg.ReportInterval, "r", time.Duration(10)*time.Second, "Report interval in seconds")
+	flag.DurationVar(&cfg.PoolInterval, "p", time.Duration(2)*time.Second, "Poll interval in seconds")
+	flag.Parse()
+	if err := env.Parse(&cfg); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	cfg.EndPoint = "http://" + cfg.Address + "/update/"
+
+	return &cfg, nil
+}
--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,34 +2,14 @@ package main
 
 import (
 	"bytes"
-	"flag"
+	"github.com/IlyaYP/devops/cmd/agent/config"
 	"github.com/IlyaYP/devops/internal"
-	"github.com/caarlos0/env/v6"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
-
-//type config struct {
-//	Address        string `env:"ADDRESS" envDefault:"localhost:8080"`
-//	ReportInterval int    `env:"REPORT_INTERVAL" envDefault:"10"`
-//	PoolInterval   int    `env:"POLL_INTERVAL" envDefault:"2"`
-//}
-
-//type config struct {
-//	Address        string `env:"ADDRESS"`
-//	ReportInterval int    `env:"REPORT_INTERVAL"`
-//	PoolInterval   int    `env:"POLL_INTERVAL"`
-//}
-type config struct {
-	Address        string        `env:"ADDRESS"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
-	PoolInterval   time.Duration `env:"POLL_INTERVAL"`
-}
-
-var cfg config
 
 func main() {
 	if err := run(); err != nil {
@@ -38,34 +18,29 @@ func main() {
 }
 
 func run() error {
-	flag.StringVar(&cfg.Address, "a", "localhost:8080", "Server address")
-	flag.DurationVar(&cfg.ReportInterval, "r", time.Duration(10)*time.Second, "Report interval in seconds")
-	flag.DurationVar(&cfg.PoolInterval, "p", time.Duration(2)*time.Second, "Poll interval in seconds")
-	flag.Parse()
-	if err := env.Parse(&cfg); err != nil {
+	cfg, err := config.LoadConfig()
+	if err != nil {
 		log.Println(err)
 		return err
 	}
+
 	log.Println("Agent start using args:ADDRESS", cfg.Address, "REPORT_INTERVAL",
 		cfg.ReportInterval, "POLL_INTERVAL", cfg.PoolInterval)
-	pollInterval := cfg.PoolInterval
-	reportInterval := cfg.ReportInterval
 
-	endPoint := "http://" + cfg.Address + "/update/"
 	quit := make(chan os.Signal, 2)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	var buf bytes.Buffer
 
 	getMetrics := internal.NewMonitor(&buf)
-	poll := time.Tick(pollInterval)
-	report := time.Tick(reportInterval)
+	poll := time.Tick(cfg.PoolInterval)
+	report := time.Tick(cfg.ReportInterval)
 breakFor:
 	for {
 		select {
 		case <-poll:
 			getMetrics()
 		case <-report:
-			if err := internal.SendBufRetry(endPoint, &buf); err != nil {
+			if err := internal.SendBufRetry(cfg.EndPoint, &buf); err != nil {
 				log.Println(err)
 				log.Println("Ok, let's try again later")
 			}
